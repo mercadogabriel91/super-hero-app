c907f880dac6fee805b6b45cffb436b8fa06027b
Gab <mercado.gabriel91@hotmail.com>
2021-05-14 22:04:01 -0300

    Feat: team modal incomplete

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,32 +1,38 @@
 import React from 'react';
 // import Modal from '@material-ui/core/Modal';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { Modal, Backdrop, Fade } from "@material-ui/core";
+
+//Local imports 
+import TeamForm from '../../views/TeamForm/TeamForm';
 
 export default function SimpleModal(props) {
 
     const classes = useStyles();
 
     function getModalStyle() {
-        const top = 25;
+        const top = 2;
         const left = 25;
 
         return {
             top: `${top}%`,
             margin: 'auto',
-            width: '50%',
-            height: '60%'
+            width: '90%',
+            height: '96%',
+            backgroundColor: 'black',
+            border: '2px solid red',
+            borderRadius: 10
         };
     }
     const [modalStyle] = React.useState(getModalStyle);
 
     const body = (
         <div style={modalStyle} className={classes.paper}>
-            <h2 id="simple-modal-title">Text in a modal</h2>
+            {<TeamForm />}
+            {/* <h2 id="simple-modal-title">Text in a modal</h2>
             <p id="simple-modal-description">
                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </p>
+            </p> */}
         </div>
     );
 
@@ -43,7 +49,6 @@ export default function SimpleModal(props) {
             }}
         >
             {body}
-           
         </Modal>
     )
 }
@@ -55,6 +60,6 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.background.paper,
         border: '2px solid #000',
         boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
+        //padding: theme.spacing(2, 4, 3),
     },
 }));
---
+++ src/views/TeamForm/TeamForm.js
@@ -0,0 +1,110 @@
+import React from 'react';
+//Material ui
+import { makeStyles } from '@material-ui/core/styles';
+import { AccessAlarm, DeleteForever } from '@material-ui/icons';
+//Local imports
+import LoadingBar from '../../components/LoadingBar/LoadingBar';
+
+const TeamFormElem = (props) => {
+    const { classes } = props;
+    return (
+        <div>
+            <div className={classes.teamTitleContainer}>
+                {props.alignment === 'good' ? <p className={classes.goodTeamTitle}>Good Team</p> : <p className={classes.badTeamTitle}>Bad Team</p>}
+            </div>
+            <div className={classes.topTeamImageContainer}>
+                <div className={classes.heroImage}>
+                    <div className={classes.heroNameContainer}>
+                        {props.alignment === 'good' ? <p className={classes.heroName}>Hero name</p> : <p className={classes.badHeroName}>Hero name</p>}
+                    </div>
+                    <div style={{display:'flex', flexDirection:'row', justifyContent:'center',paddingTop:'20vh',}}>
+                        {/* <AccessAlarm style={{ color: 'white', marginRight:10 }} /> */}
+                        <div style={{border:'1px solid red', borderRadius:50, width:27, height:27, padding:2, alignItems:'center', justifyContent:'center', display:'flex' }}><DeleteForever style={{ color: 'red'}} /></div>
+                    </div>
+                </div>
+                <div className={classes.heroImage}>
+                    <div className={classes.heroNameContainer}>
+                        {props.alignment === 'good' ? <p className={classes.heroName}>Hero name</p> : <p className={classes.badHeroName}>Hero name</p>}
+                    </div>
+                </div>
+                <div className={classes.heroImage}>
+                    <div className={classes.heroNameContainer}>
+                        {props.alignment === 'good' ? <p className={classes.heroName}>Hero name</p> : <p className={classes.badHeroName}>Hero name</p>}
+                    </div>
+                </div>
+            </div>
+            <p className={classes.teamStat}>Random stat</p>
+            <LoadingBar val={30} />
+            <p className={classes.teamStat}>Random stat</p>
+            <LoadingBar val={46} />
+            <p className={classes.teamStat}>Random stat</p>
+            <LoadingBar val={27} />
+        </div>
+    )
+}
+
+export default function TeamForm() {
+
+    const classes = useStyles();
+    return (
+        <div>
+            <TeamFormElem classes={classes} alignment={'good'} />
+            <TeamFormElem classes={classes} alignment={'evil'} />
+        </div>
+    )
+}
+
+const useStyles = makeStyles({
+    teamTitleContainer: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    goodTeamTitle: {
+        padding: 0,
+        margin: 5,
+        color: '#5daa9a'
+    },
+    badTeamTitle: {
+        color: 'red',
+        padding: 0,
+        margin: 5,
+    },
+    topTeamImageContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        margin: 0,
+        padding: 0,
+        justifyContent: 'space-evenly'
+    },
+    heroImage: {
+        backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, .99), rgba(255, 255, 255, 0.1)), url(https://www.superherodb.com/pictures2/portraits/10/100/85.jpg)`,
+        height: '30vh',
+        width: '25vw',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        justifyContent: 'center'
+    },
+    heroName: {
+        fontSize: 12,
+        color: '#5daa9a',
+        fontWeight: 'bolder',
+    },
+    badHeroName: {
+        fontSize: 12,
+        color: 'red',
+        fontWeight: 'bolder',
+    },
+    heroNameContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    teamStat: {
+        color: '#5daa9a',
+        marginLeft: '5%',
+        fontSize: 12,
+        marginBottom: 2,
+        marginTop: 2
+    }
+})
